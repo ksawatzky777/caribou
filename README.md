b593896a25f8ab3fdcbb142c02201e235de1b059
ksawatzky777 <kevin.sawatzky@ontariotechu.net>
2020-06-08 11:53:20 -0700

    Initial files

---
+++ README.md
@@ -0,0 +1,6 @@
+caribou
+=====
+
+"Fork caribou" to create a new MOOSE-based application.
+
+For more information see: [http://mooseframework.org/create-an-app/](http://mooseframework.org/create-an-app/)
---
+++ test/src/base/caribouTestApp.C
@@ -0,0 +1,62 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+#include "caribouTestApp.h"
+#include "caribouApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "MooseSyntax.h"
+#include "ModulesApp.h"
+
+InputParameters
+caribouTestApp::validParams()
+{
+  InputParameters params = caribouApp::validParams();
+  return params;
+}
+
+caribouTestApp::caribouTestApp(InputParameters parameters) : MooseApp(parameters)
+{
+  caribouTestApp::registerAll(
+      _factory, _action_factory, _syntax, getParam<bool>("allow_test_objects"));
+}
+
+caribouTestApp::~caribouTestApp() {}
+
+void
+caribouTestApp::registerAll(Factory & f, ActionFactory & af, Syntax & s, bool use_test_objs)
+{
+  caribouApp::registerAll(f, af, s);
+  if (use_test_objs)
+  {
+    Registry::registerObjectsTo(f, {"caribouTestApp"});
+    Registry::registerActionsTo(af, {"caribouTestApp"});
+  }
+}
+
+void
+caribouTestApp::registerApps()
+{
+  registerApp(caribouApp);
+  registerApp(caribouTestApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+// External entry point for dynamic application loading
+extern "C" void
+caribouTestApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  caribouTestApp::registerAll(f, af, s);
+}
+extern "C" void
+caribouTestApp__registerApps()
+{
+  caribouTestApp::registerApps();
+}
