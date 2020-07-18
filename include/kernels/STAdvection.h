d9a4b2d2213fe0c98fb4f0e6f854cc6d29bd51b0
ksawatzky777 <kevin.sawatzky@ontariotechu.net>
2020-07-18 06:35:57 -0700

    Changed the advection kernel to implement the full upwinding scheme.

--- include/kernels/STAdvection.h
+++ include/kernels/STAdvection.h
@@ -1,19 +1,63 @@
+//* This file is a modification of the ConservativeAdvection kernel.
+//* It utilizes a velocity profile provided by the material system as opposed
+//* to a constant velocity provided by the input parameters system.
+
+//* This kernel is still in developement and is to be used temporarily while
+//* a more elegent method of converting the ConservativeAdvection kernel to use
+//* the material system is developed.
+
 #pragma once
+
 #include "Kernel.h"
 
+// Forward Declaration
 class STAdvection;
 
+/**
+ * Advection of the variable by the velocity provided by the material system.
+ * Options for numerical stabilization are: none; full upwinding
+ */
 template <>
 InputParameters validParams<STAdvection>();
 
 class STAdvection : public Kernel
 {
 public:
+  static InputParameters validParams();
+
   STAdvection(const InputParameters & parameters);
 
 protected:
   virtual Real computeQpResidual() override;
   virtual Real computeQpJacobian() override;
+  virtual void computeResidual() override;
+  virtual void computeJacobian() override;
 
+  /// advection velocity from the material system
   const MaterialProperty<RealVectorValue> & _velocity;
+
+  /// enum to make the code clearer
+  enum class JacRes
+  {
+    CALCULATE_RESIDUAL = 0,
+    CALCULATE_JACOBIAN = 1
+  };
+
+  /// Type of upwinding
+  const enum class UpwindingType { none, full } _upwinding;
+
+  /// Nodal value of u, used for full upwinding
+  const VariableValue & _u_nodal;
+
+  /// In the full-upwind scheme, whether a node is an upwind node
+  std::vector<bool> _upwind_node;
+
+  /// In the full-upwind scheme d(total_mass_out)/d(variable_at_node_i)
+  std::vector<Real> _dtotal_mass_out;
+
+  /// Returns - _grad_test * velocity
+  Real negSpeedQp() const;
+
+  /// Calculates the fully-upwind Residual and Jacobian (depending on res_or_jac)
+  void fullUpwind(JacRes res_or_jac);
 };
