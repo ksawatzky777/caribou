a281d28cee8ab18f3b6d1853b80757118f89e431
ksawatzky777 <kevin.sawatzky@ontariotechu.net>
2020-07-19 15:38:06 -0700

    Added a material class which supplies properties for extended ST.

---
+++ include/materials/GenericCaribouMaterial.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "STMaterial.h"
+
+class GenericCaribouMaterial;
+
+/**
+ * The material which provides all the constants and parameters required for
+ * CARIBOU's atmospheric dispersion model.
+ */
+
+template <>
+InputParameters validParams<GenericCaribouMaterial>();
+
+class GenericCaribouMaterial : public STMaterial
+{
+public:
+  GenericCaribouMaterial(const InputParameters & parameters);
+
+protected:
+
+  virtual void computeQpProperties() override;
+
+  /// Decay constant this material is providing.
+  MaterialProperty<Real> & _decay_const;
+
+  /// Settling velocity this material is providing.
+  MaterialProperty<RealVectorValue> & _settling_v;
+
+  /// Wet scavenging coefficient this material is providing.
+  MaterialProperty<Real> & _wet_scavenge;
+};
---
+++ src/kernels/GenericCaribouMaterial.C
@@ -0,0 +1,41 @@
+#include "GenericCaribouMaterial.h"
+
+registerMooseObject("caribouApp", GenericCaribouMaterial);
+
+template <>
+InputParameters
+validParams<GenericCaribouMaterial>()
+{
+  InputParameters params = validParams<STMaterial>();
+  params.addClassDescription("Generic CARIBOU material which provides constant"
+                             " parameters required to implement the atmospheric"
+                             " dispersion model.");
+  params.addRequiredParam<Real>("decay_constant", "Decay constant for this "
+                                "radionuclide.");
+  params.addParam<Real>("settling_velocity", 0.0, "The z component of the "
+                        "settling velocity for this radionuclide.");
+  params.addParam<Real>("wet_scavenge_constant", 0.0, "The scavenging "
+                        "coefficient for wet deposition.");
+  return params;
+}
+
+GenericCaribouMaterial::GenericCaribouMaterial(const InputParameters & parameters)
+  : STMaterial(parameters),
+    _decay_const(declareProperty<Real>("decay_constant")),
+    _settling_v(declareProperty<RealVectorValue>("settling_velocity")),
+    _wet_scavenge(declareProperty<Real>("wet_scavenge_constant"))
+{
+}
+
+void
+GenericCaribouMaterial::computeQpProperties()
+{
+  /// Compute properties required for the base scalar transport kernels.
+  STMaterial::computeQpProperties();
+
+  /// Compute properties for additional sinks and sources in the scalar transport
+  /// equation.
+  _decay_const[_qp] = getParam<Real>("decay_constant");
+  _settling_v[_qp] = {0.0, 0.0, getParam<Real>("settling_velocity")};
+  _wet_scavenge[_qp] = getParam<Real>("wet_scavenge_constant");
+}
