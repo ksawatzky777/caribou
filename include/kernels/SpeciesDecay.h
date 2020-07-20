3749702dafa78ff6d8a6c14da8af8b1c7eaa5bb6
ksawatzky777 <kevin.sawatzky@ontariotechu.net>
2020-07-20 10:23:26 -0700

    Updated formatting.

--- include/kernels/SpeciesDecay.h
+++ include/kernels/SpeciesDecay.h
@@ -12,17 +12,17 @@ class SpeciesDecay;
  template <>
  InputParameters validParams<SpeciesDecay>();
 
- class SpeciesDecay : public Kernel
- {
- public:
-   static InputParameters validParams();
+class SpeciesDecay : public Kernel
+{
+public:
+  static InputParameters validParams();
 
-   SpeciesDecay(const InputParameters & parameters);
+  SpeciesDecay(const InputParameters & parameters);
 
- protected:
-   virtual Real computeQpResidual() override;
-   virtual Real computeQpJacobian() override;
+protected:
+  virtual Real computeQpResidual() override;
+  virtual Real computeQpJacobian() override;
 
-   /// Decay constant provided by the material system.
-   const MaterialProperty<Real> & _decay_const;
- };
+  /// Decay constant provided by the material system.
+  const MaterialProperty<Real> & _decay_const;
+};
