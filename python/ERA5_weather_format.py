f656454010b16431a6eaef3c9add0f940ef99f4c
ksawatzky777 <kevin.sawatzky@ontariotechu.net>
2020-08-03 09:14:04 -0700

    Updated data formatting scheme.

--- python/ERA5_weather_format.py
+++ python/ERA5_weather_format.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This is a data formatting script which extracts long-lat data from ERA5 GRIB1 
+This is a data formatting script which extracts long-lat data from ERA5 GRIB1
 and GRIB2 files available in the climate data store and than convert the lat-
 long data to a cartesian coordinate system suitable for CARIBOU input.
 """
@@ -11,190 +11,290 @@ import os
 import barometric_utils as bu
 import csv
 
-def convert_by_latlong(file, point1, point2, p_level, deltas=[31, 31]):
+def convert_by_latlong(file, point1, point2, p_level,
+                       times, deltas=[31, 31]):
     """
-    This function extracts long-lat data within a bounded region defined by 
-    the input parameters. It than converts the long/lat coordinates to an 
-    x-y-z cartesian grid where (0,0,0) is located at point1. The resulting 
-    coordinate transformed data is output to three csv files formatted to 
-    become initial conditions for CARIBOU executions. Latitude must range 
-    between [90, -90], where 0 is the equator. Longitude must range 
+    This function extracts long-lat data within a bounded region defined by
+    the input parameters. It than converts the long/lat coordinates to an
+    x-y-z cartesian grid where (0,0,0) is located at point1. The resulting
+    coordinate transformed data is output to three csv files formatted to
+    become initial conditions for CARIBOU executions. Latitude must range
+    between [90, -90], where 0 is the equator. Longitude must range
     between [0, 360], where 0 is the Prime Meridian.
-    
+
     Input parameters:
         file: string of the filename.
-        point1: python list where entry 0 is the latitude and entry 1 is the 
+        point1: python list where entry 0 is the latitude and entry 1 is the
         longitude.
-        point2: python list where entry 0 is the latitude and entry 1 is the 
+        point2: python list where entry 0 is the latitude and entry 1 is the
         longitude.
         p_level: python list of isobaric pressure levels.
-        deltas: python list of coordinate transformation factors to convert 
-        lat/long values to x (entry 0) and y (entry 1) values. By default, 
-        this is 31 km between datapoints in both lat and long (default for 
-        ERA5 reanalysis). 
+        times: python list of times the data is to be extracted from.
+        deltas: python list of coordinate transformation factors to convert
+        lat/long values to x (entry 0) and y (entry 1) values. By default,
+        this is 31 km between datapoints in both lat and long (default for
+        ERA5 reanalysis).
     Outputs:
-        u.csv (csv file containing the magnitude of the east-facing wind 
+        u.csv (csv file containing the magnitude of the east-facing wind
         component, m/s).
-        v.csv (csv file containing the magnitude of the north-facing wind 
+        v.csv (csv file containing the magnitude of the north-facing wind
         component), m/s.
-        w.csv (csv file containing the magnitude of the updraft velocity, 
+        w.csv (csv file containing the magnitude of the updraft velocity,
         Pa/s).
-        All output csv files contain the x-y-z coordinates of the data in the 
-        first three columns, with the data following in column four.
+        coords.csv (csv file containing the coordinate data. X is column 1, y is
+        column 2, z is column 3 and t is column 4).
+
+        All output csv files containing weather data use a single column
+        format which is accepted by the MOOSE TrilinearInterpolation object
+        (required for CARIBOU's scalar transport kernels). If more than 1
+        timestep of the velocity vector is required, the data for the additional
+        timesteps are adjoined as columns with a header containing the name of
+        the data and its timestep (Ex. w3: where w is the data type and 3 is the
+        timestep).
+
+        coords.csv is formatted such that each column has the same number of
+        rows (due to the limitation of the csv file format). The largest
+        dimension is the only dimension filled completely with real coordinate
+        values, the remainder are filled with zeros and actual coordinate
+        values. CARIBOU's file reading scheme can decode the data and remove
+        these superfluous zeroes (using the GenericCaribousMaterial or the
+        STMaterial materials).
     """
-    
-    #FIX COORDS OUTPUT, CAN'T USE PANDAS
-    
+
     #Declaring output directory
     output_dir = './'
-    
+
+    print ('Opening file.')
     #Open the GRIB file.
     grbs = pygrib.open(file)
-    
-    #Designating GRIB messages at the initial pressure level (for fetching 
-    #lats and longs).
-    grb_u = grbs.select(name='U component of wind',
-                        typeOfLevel='isobaricInhPa', level=p_level[0])[0]
-    grb_v = grbs.select(name='V component of wind',
-                        typeOfLevel='isobaricInhPa', level=p_level[0])[0]
-    grb_w = grbs.select(name='Vertical velocity', 
-                        typeOfLevel='isobaricInhPa', level=p_level[0])[0]
-    
-    #Fetch data from the GRIB file and assign it to a numpy array for the 
-    #first pressure level.
-    u, lats, longs = grb_u.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-    v, lats, longs = grb_v.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-    w, lats, longs = grb_w.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-    
-    #Declaring numpy arrays for the transformed coordinate system.
-    grid_x = np.ones_like(longs)
-    grid_y = np.ones_like(lats)
-    
-    #Declaring numpy arrays for additional coordinate output.
-    x_out = np.ones(np.shape(longs)[0])
-    y_out = np.ones(np.shape(lats)[1])
-    z_out = np.ones(len(p_level))
-    
-    #Declaring the z coordinates for additional coordinate output.
-    for i in range(len(p_level)):
-        z_out[i] = bu.barometric_height(p_level[i])*1000
 
-    #Generating the 2-D cartesian grid from lats and longs.
-    for i in range(np.shape(longs)[0]):
-        x_out[i] = i*(deltas[0])*(1000)
-        for j in range(np.shape(longs)[1]):
-            grid_x[i,j] = i*(deltas[0])*(1000)
-    
-    for j in range(np.shape(lats)[1]):
-        y_out[j] = (j)*(deltas[1])*(1000)
-        for i in range(np.shape(lats)[0]):
-            i_ = (np.shape(lats)[0] - i - 1)
-            j_ = (np.shape(lats)[1] - j - 1)
-            grid_y[i_,j_] = (j_)*(deltas[1])*(1000)
-    
-    #Flatten both x and y grid components into 1-D arrays
-    flat_x = grid_x.flatten()
-    flat_y = grid_y.flatten()
-    
-    #Building the first z component array.
-    flat_z_1 = np.full_like(flat_x, (bu.barometric_height(p_level[0]))*1000)
-    
-    #Building initial dictionaries for the dataframes.
-    coords = {'x': flat_x, 'y': flat_y, 'z': flat_z_1}
-    
-    #Building 4 pandas dataframes to store the data.
-    df_u = pd.DataFrame(data=coords)
-    df_v = pd.DataFrame(data=coords)
-    df_w = pd.DataFrame(data=coords)
-    
-    #Building 3 pandas dataframes for temporary data storage.
-    t_coords = {'x': flat_x, 'y': flat_y}
-    t_df_u = pd.DataFrame(data=t_coords)
-    t_df_v = pd.DataFrame(data=t_coords)
-    t_df_w = pd.DataFrame(data=t_coords)
-    
-    #Designating a GRIB message for each velocity component at each pressure 
-    #level.
+    #Fetch initial latitudes and longitudes for looping purposes.
     grb_u = grbs.select(name='U component of wind',
-                            typeOfLevel='isobaricInhPa',
-                            level=p_level[0])[0]
-    grb_v = grbs.select(name='V component of wind',
-                            typeOfLevel='isobaricInhPa',
-                            level=p_level[0])[0]
-    grb_w = grbs.select(name='Vertical velocity', 
-                            typeOfLevel='isobaricInhPa',
-                            level=p_level[0])[0]
-    
-    #Fetch data from the GRIB file and assign it to a numpy array for each
-    #pressure level.
-    u, lats, longs = grb_u.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-    v, lats, longs = grb_v.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-    w, lats, longs = grb_w.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-    
-    #Adjoining data to the end of the dataframes.
-    df_u["data"] = u.flatten()
-    df_v["data"] = v.flatten()
-    df_w["data"] = w.flatten()
-    
-    #Loop through each pressure level other than the first.
-    for i in range(len(p_level) - 1):
-        #Designate GRIB messages for each velocity component at each pressure 
-        #level.
-        grb_u = grbs.select(name='U component of wind',
-                            typeOfLevel='isobaricInhPa',
-                            level=p_level[i + 1])[0]
-        grb_v = grbs.select(name='V component of wind',
-                            typeOfLevel='isobaricInhPa',
-                            level=p_level[i + 1])[0]
-        grb_w = grbs.select(name='Vertical velocity', 
-                            typeOfLevel='isobaricInhPa',
-                            level=p_level[i + 1])[0]
+                        typeOfLevel='isobaricInhPa',
+                        level=p_level[0])[0]
 
-        #Fetch data from the GRIB file and assign it to a numpy array for each
-        #pressure level.
-        u, lats, longs = grb_u.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-        v, lats, longs = grb_v.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-        w, lats, longs = grb_w.data(lat1=point1[0], lat2=point2[0], 
-                                    lon1=point1[1], lon2=point2[1])
-        
-        #Generating a flattened z coordinate.
-        flat_z = np.full_like(flat_x, 
-                              bu.barometric_height(p_level[i + 1]))*1000
-        
-        #Storing flattened data to the temporary dataframes.
-        t_df_u["z"] = flat_z
-        t_df_v["z"] = flat_z
-        t_df_w["z"] = flat_z
-        
-        t_df_u["data"] = u.flatten()
-        t_df_v["data"] = v.flatten()
-        t_df_w["data"] = w.flatten()
-        
+    temp_u, lats, longs = grb_u.data(lat1=point1[0], lat2=point2[0],
+                                     lon1=point1[1], lon2=point2[1])
+
+    print ('Building coordinate system.')
+    #Building cartesian coordinate system from the latitudes, longitudes and
+    #pressure levels.
+    grid_x = np.zeros(np.shape(longs)[0])
+    grid_y = np.zeros(np.shape(lats)[1])
+    grid_z = np.zeros(len(p_level))
+    grid_t = np.zeros(len(times))
+
+    for x in range(np.shape(longs)[0]):
+        grid_x[x] = x*deltas[0]
+
+    for y in range(np.shape(lats)[1]):
+        grid_y[y] = y*deltas[1]
+
+    for z in range(len(p_level)):
+        grid_z[z] = bu.barometric_height(p_level[z])*1000
+
+    for t in range(len(times)):
+        grid_t[t] = times[t]
+
+    #Filling the 3 smallest dimensions with zeroes to match the largest.
+    largest = 0
+    if np.shape(grid_x)[0] > largest:
+        largest = np.shape(grid_x)[0]
+
+    if np.shape(grid_y)[0] > largest:
+        largest = np.shape(grid_y)[0]
+
+    if np.shape(grid_z)[0] > largest:
+        largest = np.shape(grid_z)[0]
+
+    if np.shape(grid_t)[0] > largest:
+        largest = np.shape(grid_t)[0]
+
+    out_x = np.zeros(largest)
+    out_y = np.zeros(largest)
+    out_z = np.zeros(largest)
+    out_t = np.zeros(largest)
+
+    for x in range(np.shape(longs)[0]):
+        out_x[x] = grid_x[x]
+
+    for y in range(np.shape(lats)[1]):
+        out_y[y] = grid_y[y]
+
+    for z in range(len(p_level)):
+        out_z[z] = grid_z[z]
+
+    for t in range(len(times)):
+        out_t[t] = grid_t[t]
+
+    #Building a pandas dataframe for the output coordinates.
+    coords = {'x': out_x, 'y': out_y, 'z': out_z, 't': out_t}
+
+    df_coords = pd.DataFrame(data=coords)
+
+    print ('Extracting initial data at ' + str(times[0]) + ' (time zero).')
+    #Loop over the data at the initial time index.
+    #Preallocating for speed.
+    data_size = np.shape(grid_x)[0]*np.shape(grid_y)[0]*np.shape(grid_z)[0]
+    u = np.zeros(data_size)
+    v = np.zeros(data_size)
+    w = np.zeros(data_size)
+
+    #Counter for the master array position.
+    counter = 0
+
+    #Loop over the data for u.
+    for x in range(np.shape(longs)[0]):
+        for y in range(np.shape(lats)[1]):
+            for z in range(len(p_level)):
+
+                #Fetch grib message for u.
+                grb_u = grbs.select(name='U component of wind',
+                                    typeOfLevel='isobaricInhPa',
+                                    level=p_level[z])[0]
+                #Extract the data for u (lats and longs come along for the
+                #ride).
+                temp_u, lats, longs = grb_u.data(lat1=point1[0],
+                                                 lat2=point2[0],
+                                                 lon1=point1[1],
+                                                 lon2=point2[1])
+                #Collapse the data into a 1-D array as per MOOSE
+                #TrilinearInterpolation specification.
+                u[counter] = temp_u[x,y]
+                counter += 1
+
+    print ('U completed at ' + str(times[0]) + ' (time 0).')
+    counter = 0
+
+    #Loop over the data for v. Same process as u.
+    for x in range(np.shape(longs)[0]):
+        for y in range(np.shape(lats)[1]):
+            for z in range(len(p_level)):
+
+                grb_v = grbs.select(name='V component of wind',
+                                    typeOfLevel='isobaricInhPa',
+                                    level=p_level[z])[0]
+                temp_v, lats, longs = grb_u.data(lat1=point1[0],
+                                                 lat2=point2[0],
+                                                 lon1=point1[1],
+                                                 lon2=point2[1])
+                v[counter] = temp_v[x,y]
+                counter += 1
+
+    print ('V completed at ' + str(times[0]) + ' (time 0).')
+    counter = 0
+
+    #Loop over the data for w. Same process as u.
+    for x in range(np.shape(longs)[0]):
+        for y in range(np.shape(lats)[1]):
+            for z in range(len(p_level)):
+
+                grb_w = grbs.select(name='Vertical velocity',
+                                    typeOfLevel='isobaricInhPa',
+                                    level=p_level[z])[0]
+                temp_w, lats, longs = grb_u.data(lat1=point1[0],
+                                                 lat2=point2[0],
+                                                 lon1=point1[1],
+                                                 lon2=point2[1])
+                w[counter] = bu.vert_to_w(temp_w[x,y])
+                counter += 1
+
+    print ('W completed at ' + str(times[0]) + ' (time 0).')
+
+    #Building pandas dataframes for each component of the velocity.
+    df_u = pd.DataFrame(u, columns=['u0'])
+    df_v = pd.DataFrame(v, columns=['v0'])
+    df_w = pd.DataFrame(w, columns=['w0'])
+
+    #Loop over each time index other than the first.
+    for t in range(len(times) - 1):
+        print ('Extracting data at ' + str(times[t]) +
+               ' (time ' + str(t) + ').')
+
+        data_size = np.shape(grid_x)[0]*np.shape(grid_y)[0]*np.shape(grid_z)[0]
+        u = np.zeros(data_size)
+        v = np.zeros(data_size)
+        w = np.zeros(data_size)
+
+        counter = 0
+
+        #Loop over the data for u.
+        for x in range(np.shape(longs)[0]):
+            for y in range(np.shape(lats)[1]):
+                for z in range(len(p_level)):
+                    grb_u = grbs.select(name='U component of wind',
+                                        typeOfLevel='isobaricInhPa',
+                                        level=p_level[z])[t + 1]
+                    temp_u, lats, longs = grb_u.data(lat1=point1[0],
+                                                     lat2=point2[0],
+                                                     lon1=point1[1],
+                                                     lon2=point2[1])
+
+                    u[counter] = temp_u[x,y]
+                    counter += 1
+
+        print ('U completed at ' + str(times[t]) + ' (time ' + str(t) + ').')
+        counter = 0
+
+        #Loop over the data for v.
+        for x in range(np.shape(longs)[0]):
+            for y in range(np.shape(lats)[1]):
+                for z in range(len(p_level)):
+
+                    grb_v = grbs.select(name='V component of wind',
+                                        typeOfLevel='isobaricInhPa',
+                                        level=p_level[z])[t + 1]
+                    temp_v, lats, longs = grb_u.data(lat1=point1[0],
+                                                     lat2=point2[0],
+                                                     lon1=point1[1],
+                                                     lon2=point2[1])
+                    v[counter] = temp_v[x,y]
+                    counter += 1
+
+        print ('V completed at ' + str(times[t]) + ' (time ' + str(t) + ').')
+        counter = 0
+
+        #Loop over the data for w.
+        for x in range(np.shape(longs)[0]):
+            for y in range(np.shape(lats)[1]):
+                for z in range(len(p_level)):
+
+                    grb_w = grbs.select(name='Vertical velocity',
+                                        typeOfLevel='isobaricInhPa',
+                                        level=p_level[z])[t + 1]
+                    temp_w, lats, longs = grb_u.data(lat1=point1[0],
+                                                     lat2=point2[0],
+                                                     lon1=point1[1],
+                                                     lon2=point2[1])
+                    w[counter] = bu.vert_to_w(temp_w[x,y])
+                    counter += 1
+
+        print ('W completed at ' + str(times[t]) + ' (time ' + str(t) + ').')
+
+        t_df_u = pd.DataFrame(u, columns=['u' + str(t)])
+        t_df_v = pd.DataFrame(v, columns=['v' + str(t)])
+        t_df_w = pd.DataFrame(w, columns=['w' + str(t)])
+
         #Adjoining temporary dataframes to the output dataframes.
         df_u = df_u.append(t_df_u, ignore_index=True)
         df_v = df_v.append(t_df_v, ignore_index=True)
         df_w = df_w.append(t_df_w, ignore_index=True)
-        
+
+    #Close GRIB file
+    grbs.close()
+
     #Generating file names.
     u_out_file = os.path.join(output_dir, 'u.csv')
     v_out_file = os.path.join(output_dir, 'v.csv')
     w_out_file = os.path.join(output_dir, 'w.csv')
     coords_out_file = os.path.join(output_dir, 'coords.csv')
-    
+
+    print('Writing to csv files.')
+
     #Writing to files.
     df_u.to_csv(u_out_file, index=False)
     df_v.to_csv(v_out_file, index=False)
     df_w.to_csv(w_out_file, index=False)
-    
-    #Close GRIB file
-    grbs.close()
-    
+    df_coords.to_csv(coords_out_file, index=False)
+
     return 'Coordinate transform complete. csv files have been written.'
--- python/barometric_utils.py
+++ python/barometric_utils.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This is a utility script which converts isobaric pressure levels utilized by 
-ERA5 and MERRA-2 for non-surface computations into the respective altitudes 
+This is a utility script which converts isobaric pressure levels utilized by
+ERA5 and MERRA-2 for non-surface computations into the respective altitudes
 relative to the surface of the earth.
 """
 
@@ -10,9 +10,9 @@ import math
 
 def barometric_height(pressure):
     """
-    This function computes the height of a column of air above the earths 
+    This function computes the height of a column of air above the earths
     surface using the barometric formula.
-    
+
     Input parameters:
         pressure: Pressure to be converted to a height (in hPa).
     Returns:
@@ -23,19 +23,19 @@ def barometric_height(pressure):
     h_b = [11000, 20000, 32000, 47000, 51000, 71000]
     t_b = [288.15, 216.65, 216.15, 228.65, 270.65, 270.65, 214.65]
     l_b = [-0.0065, 0.0, 0.001, 0.0028, 0.0, -0.0028, -0.002]
-    
+
     #Define constants.
     g = 9.8067
     gas_const = 8.3145
     molar_mass = 0.0290
-    
-    #Determine if a given pressure is directly equivalent to a definition 
+
+    #Determine if a given pressure is directly equivalent to a definition
     #pressure.
     if pressure in p_b:
-        #If the pressure is a definition pressure, returns the related 
+        #If the pressure is a definition pressure, returns the related
         #definition height.
         return h_b[p_b.index(pressure)]/1000
-    
+
     #Determine which section of the piecewise-exponential function is relevant
     #for the given pressure.
     if pressure < p_b[0] and pressure > p_b[1]:
@@ -54,18 +54,25 @@ def barometric_height(pressure):
         b = 6
     else:
         raise ValueError('The barometric formula is not valid for this case')
-    
+
     if l_b[b] != 0.0:
-        #Compute the height of the column of air for a non-zero temperature 
+        #Compute the height of the column of air for a non-zero temperature
         #lapse rate, returning it in km.
         exponent = (gas_const*l_b[b])/(g*molar_mass)
         denom = (pressure/p_b[b])**(exponent)
-    
+
         return (h_b[b] + ((t_b[b]/denom) - t_b[b])/(l_b[b]))/1000
     else:
         #Compute the height of the column of air for a temperature lapse rate
         #which is equal to zero.
         log = math.log(pressure/(p_b[b]))
         denom = (gas_const*(t_b[b]))/((-g)*molar_mass)
-        
+
         return (log*denom + h_b[b])/1000
+
+def vert_to_w(vertical_velocity):
+    #Define constants.
+    g = 9.8067
+    rho = 1.225
+
+    return (-1)*vertical_velocity/(rho*g)
